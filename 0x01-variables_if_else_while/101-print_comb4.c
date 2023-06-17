7717d18f2ff5b156c7581a5537a912c21fe5a5b7
ameskour <ameskour97@gmail.com>
2023-06-16 19:13:08 -0700

    together

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,25 @@
-/*
- * File: 101-print_comb4.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three different digits,
+ * main - Prints all possible different combinations of two digits,
  *        in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int digit1;
+	int digit2;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
+			if (digit1 != 8 || digit2 != 9)
+			{
 				putchar(',');
 				putchar(' ');
 			}
@@ -38,3 +30,4 @@ int main(void)
 
 	return (0);
 }
+
