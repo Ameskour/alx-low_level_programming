97136881e82e2889713baedd0cfc101e35b13396
ameskour <ameskour97@gmail.com>
2023-06-29 14:02:20 -0700

    toutes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,27 +2,29 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: the destination string
- * @src: the source string
- * Return: a pointer to the resulting string dest
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, j;
+	int i, j;
 
-    /* Find the end of the dest string */
-    for (i = 0; dest[i] != '\0'; i++)
-        ;
+	i = 0;
+	j = 0;
 
-    /* Append the src string at the end of dest */
-    for (j = 0; src[j] != '\0'; j++)
-    {
-        dest[i] = src[j];
-        i++;
-    }
+	while (dest[i] != '\0')
+		i++;
 
-    /* Add the terminating null byte */
-    dest[i] = '\0';
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
-    return (dest);
+	dest[i] = '\0';
+
+	return (dest);
 }
