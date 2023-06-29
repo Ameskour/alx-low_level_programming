7986f1504bcb37628110ac2c29cc92fb583d54ca
ameskour <ameskour97@gmail.com>
2023-06-29 13:57:27 -0700

    toutes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of the variable
- * the pointer points to to 98
- * @n: pointer to the variable to update
+ * _strcat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ * Return: a pointer to the resulting string dest
  */
-void reset_to_98(int *n)
+char *_strcat(char *dest, char *src)
 {
-	*n = 98;
+    int i, j;
+
+    /* Find the end of the dest string */
+    for (i = 0; dest[i] != '\0'; i++)
+        ;
+
+    /* Append the src string at the end of dest */
+    for (j = 0; src[j] != '\0'; j++)
+    {
+        dest[i] = src[j];
+        i++;
+    }
+
+    /* Add the terminating null byte */
+    dest[i] = '\0';
+
+    return (dest);
 }
